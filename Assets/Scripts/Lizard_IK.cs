c34b2613b7251ac5ead089a63b94a11569c1df6c
Rory <54592272+RoryCSI@users.noreply.github.com>
2021-11-30 19:53:21 -0500

    Completed Lizard neck-lookAt solver.

--- Assets/Scripts/Lizard_IK.cs
+++ Assets/Scripts/Lizard_IK.cs
@@ -8,7 +8,6 @@ public class Lizard_IK : MonoBehaviour
 
     public GameObject lookat;
 
-    public Vector3 crossUp;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +22,12 @@ public class Lizard_IK : MonoBehaviour
 
     private void LateUpdate()
     {
+        solveNeck();
+    }
+
+    private void solveNeck()
+    {
         Vector3 towardObjectFromHead = lookat.transform.position - neck.transform.position;
-        towardObjectFromHead = Vector3.Cross(transform.up, towardObjectFromHead);
-        neck.transform.rotation = Quaternion.LookRotation(towardObjectFromHead, neck.transform.up);
+        neck.transform.rotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
     }
 }
